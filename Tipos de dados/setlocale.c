222cec71ab1c552bc5b2baf7dda8e72ef7e59fa4
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-11-25 13:10:24 -0300

    SetLocale

--- Tipos de dados/setlocale.c
+++ Tipos de dados/setlocale.c
@@ -5,11 +5,25 @@
 void main()
 {
     // Permite usar acentos
-    setlocale(LC_ALL, "Portuguese");
-    printf("Ol√°! \n ");
+    setlocale(LC_ALL, "");
+    printf("Ol·! \n ");
 
+    // Lendo valor inteiro
     int a = 50;
-    printf("O valor de a √© = %d \n", a);
+    printf("O valor de a È = %d \n", a);
     scanf("%d", &a);
     printf("O valor de a mudou para %d", a);
+
+    // Lendo valor quebrado
+    float b = 5.5;
+    printf("O valor de b È = %f \n", b);
+    scanf("%f", &b);
+    printf("O valor de b mudou para %f", b);
+
+    // Lendo letra
+    char letra = 't';
+    printf("O valor de c È = %c \n", letra);
+    fflush(stdin);
+    scanf("%c", &letra);
+    printf("O valor de c mudou para %c", letra);
 }
